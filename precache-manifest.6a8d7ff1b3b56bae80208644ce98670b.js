74f65b16f373f89c681038be01c91f504bb65da1
Rafler <amuronyk@gmail.com>
2019-11-04 19:47:32 +0200

    Updates

---
+++ precache-manifest.6a8d7ff1b3b56bae80208644ce98670b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ffe5d9d65db955750cd5d25def748ea",
+    "url": "/synergy_way/index.html"
+  },
+  {
+    "revision": "b7f1e8d97054cae9f4e4",
+    "url": "/synergy_way/static/css/main.c1cc9065.chunk.css"
+  },
+  {
+    "revision": "b86627a0600e761fb239",
+    "url": "/synergy_way/static/js/2.9b7c4751.chunk.js"
+  },
+  {
+    "revision": "b7f1e8d97054cae9f4e4",
+    "url": "/synergy_way/static/js/main.feeb06bd.chunk.js"
+  },
+  {
+    "revision": "06171a42bf0324affd31",
+    "url": "/synergy_way/static/js/runtime-main.6305cfd1.js"
+  }
+]);
